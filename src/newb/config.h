016b460e5e470410289dc1aef62486db02fb55b8
Coolguy <mobilemc022@gmail.com>
2026-07-30 06:26:24 +0700

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -161,9 +161,9 @@
 #define NL_CLOUD3_SHADOW 0.35             // 0.1 subtle ~ 1.0 dark
 #define NL_CLOUD3_SHADOW_OFFSET 0.3      // 0.05 minimal ~ 1.0 large
 
-#define NL_CLOUD4_SCALE 5.0               // 1.0 large ~ 20.0 tiny
+#define NL_CLOUD4_SCALE 7.0               // 1.0 large ~ 20.0 tiny
 #define NL_CLOUD4_SPEED 0.0              // 0.0 static ~ 1.0 fast moving
-#define NL_CLOUD4_THRESHOLD 0.22         // 0.0 more clouds ~ 1.0 less clouds
+#define NL_CLOUD4_THRESHOLD 0.50         // 0.0 more clouds ~ 1.0 less clouds
 #define NL_CLOUD4_SOFTNESS 0.45          // 0.05 sharp edges ~ 1.0 soft edges
 #define NL_CLOUD4_DENSITY 2.0            // 0.1 thin ~ 3.0 thick
 #define NL_CLOUD4_LAYER_GAP 0.010        // 0.001 tight layers ~ 0.05 spread layers
